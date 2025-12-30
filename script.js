21df90ed30f3f429d1b057ca0b777d52cffddcab
Moonpreneur <harsh.d@moonpreneur.com>
2025-12-30 16:46:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const PICO_IP = "http://192.168.10.68"; // CHANGE THIS
+
+function sendCmd(cmd) {
+  fetch(`${PICO_IP}/${cmd}`)
+    .catch(err => console.error(err));
+}
+
+function getADC() {
+  fetch(`${PICO_IP}/adc`)
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("adc").innerText = data;
+    })
+    .catch(err => console.error(err));
+}
+
+// Fetch ADC value every 300ms
+setInterval(getADC, 300);
