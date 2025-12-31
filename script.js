33b24f3e68a24d3e5d13e0bd5f265e7361c21245
Moonpreneur <harsh.d@moonpreneur.com>
2025-12-31 12:27:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 const PICO_IP = "https://interdependently-indecomposable-isaiah.ngrok-free.dev";
 
 function sendCmd(cmd) {
-  fetch(`${PICO_IP}/${cmd}`, {
-    headers: {
-      "ngrok-skip-browser-warning": "true"
-    }
-  }).catch(err => console.error(err));
+  fetch(`${PICO_IP}/${cmd}?ngrok-skip-browser-warning=true`)
+    .catch(err => console.error(err));
 }
 
 function getADC() {
-  fetch(`${PICO_IP}/adc`, {
-    headers: {
-      "ngrok-skip-browser-warning": "true"
-    }
-  })
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById("adc").innerText = data;
-  })
-  .catch(err => console.error(err));
+  fetch(`${PICO_IP}/adc?ngrok-skip-browser-warning=true`)
+    .then(res => res.text())
+    .then(value => {
+      console.log("ADC:", value);
+      document.getElementById("adc").innerText = value;
+    })
+    .catch(err => console.error("ADC error:", err));
 }
 
-// Update ADC every 500 ms
-setInterval(getADC, 500);
+setInterval(getADC, 400);
